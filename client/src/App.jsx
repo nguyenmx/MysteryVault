0f1ff367c26aa86f1e331cf7ef7c4d10a7e666db
Mary Nguyen <mary.nguyenwa18@gmail.com>
2024-06-20 19:53:09 -0700

    Added sign up UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,69 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Canvas} from "@react-three/fiber";
-import { useGLTF, Stage, PresentationControls} from "@react-three/drei";
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ModelCanvas from './Components/3DModels/ModelCanvas';
-
-function Model(props) {
-  const {scene} = useGLTF("/phantump_shiny.glb");
-  return <primitive object={scene} {...props}/>
-}
+import Login from './pages/Login';
 
 function App() {
-  const[users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     axios.get('http://localhost:3000/getUsers')
-    .then(users => setUsers(users.data))
-    .catch(err => console.log(err))
-  }, [])
-  return (
+      .then(response => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setError('Failed to fetch users');
+        setLoading(false);
+      });
+  }, []);
 
-    <div className= "d-flex justify-content-center align-items-center">
-      <div className= "w-50">
-      <ModelCanvas />
-     <table className = "table">
-      <thead>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Picture
-          </th>
-          <th>
-            Series
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          users.map(user => {
-            return<tr>
-              <td> {user.Name}</td>
-              <td><img src={user.Picture} alt={user.Name} style={{ width: '100px', height: 'auto' }} /></td>
-              <td> {user.Series}</td>
+  if (loading) {
+    return <div className="d-flex justify-content-center align-items-center">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="d-flex justify-content-center align-items-center">{error}</div>;
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="w-75">
+        {/* Uncomment these components if you need to use them */}
+        {/* <ModelCanvas /> */}
+        <Login />
+        <table className="table table-bordered table-striped">
+          <thead className="thead-dark">
+            <tr>
+              <th>Name</th>
+              <th>Picture</th>
+              <th>Series</th>
             </tr>
-          })
-        }
-      </tbody>
-     </table>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user.ID || index}>
+                <td>{user.Name}</td>
+                <td>
+                  <img
+                    src={user.Picture}
+                    alt={user.Name}
+                    style={{ width: '100px', height: 'auto' }}
+                  />
+                </td>
+                <td>{user.Series}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
